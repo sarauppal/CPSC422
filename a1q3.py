f92e0d20224ecb6bc66171f1d67c38fef15af559
emecke <mle_m@mac.com>
2019-09-23 19:06:59 -0700

    resolved access issue, was using constants COL ROW not variables col row

--- a1q3.py
+++ a1q3.py
@@ -19,62 +19,66 @@ OBS_MODEL = [OBS_END, OBS_1WL, OBS_2WL]
 #source https://stackoverflow.com/questions/6667201/how-to-define-a-two-dimensional-array-in-python
 TRANSFORMATION = []
 w, h = NUM_ROWS+1, NUM_COLS+1;
-TRANSFORMATION = [[0 for x in range(w)] for y in range(h)] 
-TRANSFORMATION[1][1] = {"up":    [[[1,2], 0.0], [[1,1], 0.1], [[2,1], 0.1]], 
-                        "down":  [[[1,2], 0.8], [[1,1], 0.9], [[2,1], 0.1]], 
-                        "left":  [[[1,2], 0.1], [[1,1], 0.9], [[2,1], 0.8]], 
-                        "right": [[[1,2], 0.1], [[1,1], 0.1], [[2,1], 0.0]]
-                       }
-TRANSFORMATION[1][2] = {"up":   [[[1,1], 0.8], [[1,2], 0.2], [[1,3], 0.0]], 
-                        "down": [[[1,1], 0.0], [[1,2], 0.2], [[1,3], 0.8]], 
-                        "left": [[[1,1], 0.1], [[1,2], 0.8], [[1,3], 0.1]], 
-                        "right": [[[1,1], 0.1], [[1,2], 0.8], [[1,3], 0.1]]
-                       }
-TRANSFORMATION[1][3] = {"up":   [[[1,2], 0.8], [[1,3], 0.9], [[2,3], 0.1]], 
-                        "down": [[[1,2], 0.0], [[1,3], 0.1], [[2,3], 0.1]], 
-                        "left": [[[1,2], 0.1], [[1,3], 0.9], [[2,3], 0.8]], 
-                        "right": [[[1,2], 0.1], [[1,3], 0.1], [[2,3], 0.0]]
-                       }
-TRANSFORMATION[2][1] = {"up":   [[[1,1], 0.1], [[2,1], 0.8], [[3,1], 0.1]], 
-                        "down": [[[1,1], 0.1], [[2,1], 0.8], [[3,1], 0.1]], 
-                        "left": [[[1,1], 0.0], [[2,1], 0.2], [[3,1], 0.8]], 
-                        "right":[[[1,1], 0.8], [[2,1], 0.2], [[3,1], 0.0]]
-                       }
-TRANSFORMATION[2][3] = {"up":   [[[1,3], 0.1], [[2,3], 0.8], [[3,3], 0.1]], 
-                        "down": [[[1,3], 0.1], [[2,3], 0.8], [[3,3], 0.1]], 
-                        "left": [[[1,3], 0.0], [[2,3], 0.2], [[3,3], 0.8]], 
-                        "right":[[[1,3], 0.8], [[2,3], 0.2], [[3,3], 0.0]]
-                       }
-TRANSFORMATION[3][1] = {"up":   [[[2,1], 0.1], [[3,1], 0.0], [[3,2], 0.0], [[4,1], 0.1]], 
-                        "down": [[[2,1], 0.1], [[3,1], 0.8], [[3,2], 0.8], [[4,1], 0.1]], 
-                        "left": [[[2,1], 0.0], [[3,1], 0.1], [[3,2], 0.1], [[4,1], 0.8]], 
-                        "right":[[[2,1], 0.8], [[3,1], 0.1], [[3,2], 0.1], [[4,1], 0.0]]
-                       }
-TRANSFORMATION[3][2] = {"up":   [[[3,1], 0.8], [[3,2], 0.1], [[3,3], 0.0]], 
-                        "down": [[[3,1], 0.0], [[3,2], 0.1], [[3,3], 0.8]], 
-                        "left": [[[3,1], 0.1], [[3,2], 0.8], [[3,3], 0.1]], 
-                        "right":[[[3,1], 0.1], [[3,2], 0.0], [[3,3], 0.1]]
-                       }
-TRANSFORMATION[3][3] = {"up":   [[[2,3], 0.1], [[3,3], 0.8], [[3,2], 0.8]], 
-                        "down": [[[2,3], 0.1], [[3,3], 0.0], [[3,2], 0.0]], 
-                        "left": [[[2,3], 0.0], [[3,3], 0.1], [[3,2], 0.1]], 
-                        "right":[[[2,3], 0.8], [[3,3], 0.1], [[3,2], 0.1]]
-                       },
-TRANSFORMATION[4][1] = {"up":   [[[3,1], 0.1], [[4,1], 0.1]], 
-                        "down": [[[3,1], 0.1], [[4,1], 0.9]], 
-                        "left": [[[3,1], 0.0], [[4,1], 0.1]], 
-                        "right":[[[3,1], 0.8], [[4,1], 0.9]]
-                       },
-TRANSFORMATION[4][2] = {"up":   [[[3,2], 0.1], [[4,1], 0.8]], 
-                        "down": [[[3,2], 0.1], [[4,1], 0.0]], 
-                        "left": [[[3,2], 0.0], [[4,1], 0.1]], 
-                        "right":[[[3,2], 0.8], [[4,1], 0.1]]
-                       },
-TRANSFORMATION[4][3] = {"up":   [[[3,3], 0.1]], 
-                        "down": [[[3,3], 0.1]], 
-                        "left": [[[3,3], 0.0]], 
-                        "right":[[[3,3], 0.8]]
-                       }
+TRANSFORMATION = [[{"up":    [[[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0]],
+                   "down":  [[[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0]],
+                   "left":  [[[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0]],
+                   "right": [[[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0], [[0,0], 0.0]]
+                   } for x in range(w)] for y in range(h)]
+TRANSFORMATION[1][1] = {"up":    [[[1,2], 0.0], [[1,1], 0.1], [[2,1], 0.1]],
+    "down":  [[[1,2], 0.8], [[1,1], 0.9], [[2,1], 0.1]],
+    "left":  [[[1,2], 0.1], [[1,1], 0.9], [[2,1], 0.8]],
+    "right": [[[1,2], 0.1], [[1,1], 0.1], [[2,1], 0.0]]
+    }
+TRANSFORMATION[1][2] = {"up":   [[[1,1], 0.8], [[1,2], 0.2], [[1,3], 0.0]],
+    "down": [[[1,1], 0.0], [[1,2], 0.2], [[1,3], 0.8]],
+    "left": [[[1,1], 0.1], [[1,2], 0.8], [[1,3], 0.1]],
+    "right": [[[1,1], 0.1], [[1,2], 0.8], [[1,3], 0.1]]
+    }
+TRANSFORMATION[1][3] = {"up":   [[[1,2], 0.8], [[1,3], 0.9], [[2,3], 0.1]],
+    "down": [[[1,2], 0.0], [[1,3], 0.1], [[2,3], 0.1]],
+    "left": [[[1,2], 0.1], [[1,3], 0.9], [[2,3], 0.8]],
+    "right": [[[1,2], 0.1], [[1,3], 0.1], [[2,3], 0.0]]
+    }
+TRANSFORMATION[2][1] = {"up":   [[[1,1], 0.1], [[2,1], 0.8], [[3,1], 0.1]],
+    "down": [[[1,1], 0.1], [[2,1], 0.8], [[3,1], 0.1]],
+    "left": [[[1,1], 0.0], [[2,1], 0.2], [[3,1], 0.8]],
+    "right":[[[1,1], 0.8], [[2,1], 0.2], [[3,1], 0.0]]
+    }
+TRANSFORMATION[2][3] = {"up":   [[[1,3], 0.1], [[2,3], 0.8], [[3,3], 0.1]],
+    "down": [[[1,3], 0.1], [[2,3], 0.8], [[3,3], 0.1]],
+    "left": [[[1,3], 0.0], [[2,3], 0.2], [[3,3], 0.8]],
+    "right":[[[1,3], 0.8], [[2,3], 0.2], [[3,3], 0.0]]
+    }
+TRANSFORMATION[3][1] = {"up":   [[[2,1], 0.1], [[3,1], 0.0], [[3,2], 0.0], [[4,1], 0.1]],
+    "down": [[[2,1], 0.1], [[3,1], 0.8], [[3,2], 0.8], [[4,1], 0.1]],
+    "left": [[[2,1], 0.0], [[3,1], 0.1], [[3,2], 0.1], [[4,1], 0.8]],
+    "right":[[[2,1], 0.8], [[3,1], 0.1], [[3,2], 0.1], [[4,1], 0.0]]
+    }
+TRANSFORMATION[3][2] = {"up":   [[[3,1], 0.8], [[3,2], 0.1], [[3,3], 0.0]],
+    "down": [[[3,1], 0.0], [[3,2], 0.1], [[3,3], 0.8]],
+    "left": [[[3,1], 0.1], [[3,2], 0.8], [[3,3], 0.1]],
+    "right":[[[3,1], 0.1], [[3,2], 0.0], [[3,3], 0.1]]
+    }
+TRANSFORMATION[3][3] = {"up":   [[[2,3], 0.1], [[3,3], 0.8], [[3,2], 0.8]],
+    "down": [[[2,3], 0.1], [[3,3], 0.0], [[3,2], 0.0]],
+    "left": [[[2,3], 0.0], [[3,3], 0.1], [[3,2], 0.1]],
+    "right":[[[2,3], 0.8], [[3,3], 0.1], [[3,2], 0.1]]
+        },
+TRANSFORMATION[4][1] = {"up":   [[[3,1], 0.1], [[4,1], 0.1]],
+    "down": [[[3,1], 0.1], [[4,1], 0.9]],
+    "left": [[[3,1], 0.0], [[4,1], 0.1]],
+    "right":[[[3,1], 0.8], [[4,1], 0.9]]
+        },
+TRANSFORMATION[4][2] = {"up":   [[[3,2], 0.1], [[4,1], 0.8]],
+    "down": [[[3,2], 0.1], [[4,1], 0.0]],
+    "left": [[[3,2], 0.0], [[4,1], 0.1]],
+    "right":[[[3,2], 0.8], [[4,1], 0.1]]
+        },
+TRANSFORMATION[4][3] = {"up":   [[[3,3], 0.1]],
+    "down": [[[3,3], 0.1]],
+    "left": [[[3,3], 0.0]],
+    "right":[[[3,3], 0.8]]
+    }
 
 
 
@@ -91,21 +95,21 @@ def pomdp(a, o, b = [0, 0] ):
 def updateBeliefs(a, o, b):
     evidence = o
     newBeliefState = [[0.0, 0.0, 0.0, 0.0],
-                      [0.0, 0.0, 0.0, 0.0], 
+                      [0.0, 0.0, 0.0, 0.0],
                       [0.0, 0.0, 0.0, 0.0]]
-    if o == "end":
-        evidence = 0
-    for col in range(0,NUM_COLS):
-        for row in range(0,NUM_ROWS):
-            print("col: " + str(col+1) + " row: " + str(row+1))
-            print("action: " + a)
-            prevStates = TRANSFORMATION[COL+1][ROW+1]
-            print(prevStates)
-            beliefPrevState = b[row][col]
-            probOfNewStates = sumNewStateGivenAction(a, prevStates) 
-            probEvidenceGivenNewState = OBS_MODEL[evidence][row][col]
-            print("pof " + str(evidence) + "|[" + str(col+1) + "," + str(row+1) + "] is " + str(probEvidenceGivenNewState))
-    return b
+                      if o == "end":
+                          evidence = 0
+                      for col in range(0,NUM_COLS):
+                          for row in range(0,NUM_ROWS):
+                              print("col: " + str(col+1) + " row: " + str(row+1))
+                              print("action: " + a)
+                              prevStates = TRANSFORMATION[col+1][row+1]
+                              print(prevStates)
+                              beliefPrevState = b[row][col]
+                              probOfNewStates = sumNewStateGivenAction(a, prevStates)
+                              probEvidenceGivenNewState = OBS_MODEL[evidence][row][col]
+                              print("pof " + str(evidence) + "|[" + str(col+1) + "," + str(row+1) + "] is " + str(probEvidenceGivenNewState))
+                                  return b
 
 def sumNewStateGivenAction(a, prevStates):
     sum = 0
@@ -134,17 +138,17 @@ def initBeliefs(b):
     #default for no observation
     p = 1/VALID_STATES
     bs = [[p, p, p, p],
-          [p, 0.0, p, 0.0], 
+          [p, 0.0, p, 0.0],
           [p, p, p, 0.0]]
-    #check for valid observation
-    if(validState(b)):
-        print("state is known: ")
-        print(b)
-        bs = [[0.0, 0.0, 0.0, 0.0],
-              [0.0, 0.0, 0.0, 0.0], 
-              [0.0, 0.0, 0.0, 0.0]]
-        bs[b[ROW]-1][b[COL]-1] = 1
-    return bs
+          #check for valid observation
+          if(validState(b)):
+              print("state is known: ")
+              print(b)
+              bs = [[0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0]]
+              bs[b[ROW]-1][b[COL]-1] = 1
+          return bs
 
 #prints belief array in order as defined in the lecture slides.
 def printBS(bs):
@@ -154,7 +158,7 @@ def printBS(bs):
     print("    c:1,2,3,4")
     return
 
-# calling main function with each test case where 
+# calling main function with each test case where
 print("case 1")
 BS = pomdp(["up", "up", "up"], [2,2,2])
 printBS(BS);
